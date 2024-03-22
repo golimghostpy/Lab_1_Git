820d320c526fd77a96e08c5e409c730d5b5668d2
golimghostpy <98097829+golimghostpy@users.noreply.github.com>
2024-03-22 21:57:51 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -51,6 +51,19 @@ int main()
             break;
         }
         case 2:{
+            float length, width;
+            cout << "Enter length of rectangle: ";
+            cin >> length;
+            cout << "Enter width of rectangle: ";
+            cin >> width;
+            if (length > 0 && width > 0){
+                cout << "------------------------------------" << endl;
+                cout << "Results: " << endl;
+                cout << fixed << setprecision(2) << "Perimeter: " << rectPerimetr(length, width) << endl << "Square: " << rectSquare(length, width) << endl << "Length of diagonal: " << diagonalLength(length, width) << endl;
+            }
+            else{
+                cout << "Incorrect input" << endl;
+            }
             break;
         }
         default:{
